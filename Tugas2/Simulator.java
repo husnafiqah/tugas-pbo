50bfef1b7a2b7ff7f5b6d1a6ce604cee4fe146d9
Husna Afiqah Y <89975748+husnafiqah@users.noreply.github.com>
2022-11-06 19:24:28 +0700

    Update Simulator.java

--- Tugas2/Simulator.java
+++ Tugas2/Simulator.java
@@ -3,16 +3,17 @@ public class Simulator {
         Elemen a = new Asdos ("Fairuzikum" ,24, 1); //Downcasting
         Asdos h = (Asdos) a;
 
-        Elemen b = new Dosen("Raja OP Damanik",5); //upcasting
+        Dosen b = new Dosen("Raja OP Damanik",5); //polymorphism
 
         Elemen c = new Asdos ("Angel-chan",21, 1); //upcasting
 
-        Elemen d = new Mahasiswa ("Firman", 20); //upcassting
-
+        Mahasiswa d = new Mahasiswa ("Firman", 20); //polymorphism
+        
         Elemen e = new Mahasiswa ("Nid to pass this sem", 23); //Downcasting
         Mahasiswa g = (Mahasiswa) e;
 
         Elemen f = (Elemen) new Dosen("Nivotko",3); //upcasting
+        
         
         h.send();
         b.send();

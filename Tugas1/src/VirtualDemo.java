d3c21a223c31f0e2b40f79a116d881881fc96a72
Husna Afiqah Y <89975748+husnafiqah@users.noreply.github.com>
2022-11-06 18:42:13 +0700

    Update VirtualDemo.java

--- Tugas1/src/VirtualDemo.java
+++ Tugas1/src/VirtualDemo.java
@@ -1,7 +1,7 @@
 public class VirtualDemo {
     public static void main(String[] args) {
-        Gaji s = new Gaji("Wahyu ", "KUBAR ", 3, 5000.00);
-        Pegawai e = new Gaji("Ini Nama ", "Samarinda ", 2, 2500.00);
+        Gaji s = new Gaji("Wahyu ", "KUBAR ", 3, 5000.00); //polymorphism
+        Pegawai e = new Gaji("Ini Nama ", "Samarinda ", 2, 2500.00); //down
         System.out.println("Memanggil mailCheck Berdasarkan Referensi Gaji --");
 
         s.mailCheck();
